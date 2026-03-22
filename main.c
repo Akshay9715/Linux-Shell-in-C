c4f4aace5a947560a079a13e4e32bfd875f3e170
Akshay9715 <akshaychoudhary2040@gmail.com>
2026-03-22 19:22:36 +0000

    I/O commands added

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include<unistd.h>
 #include<sys/wait.h>
 #include<string.h>
+#include<fcntl.h>
 
 #define MAX_INPUT 1024
 #define MAX_ARGS 100
@@ -65,6 +66,31 @@ int main(){
             continue;
         }
 
+        int background = 0;
+
+        if(i>0 && strcmp(args[i-1],"&")==0){
+            background = 1;
+            args[i-1] = NULL;
+        }
+
+        int input_redirect = 0, output_redirect = 0;
+        char *input_file = NULL;
+        char *output_file = NULL;
+
+        for(int j = 0;args[j]!=NULL;j++){
+            if(strcmp(args[j],"<")==0){
+                input_redirect = 1;
+                input_file = args[j+1];
+                args[j] = NULL;
+            }
+            else if(strcmp(args[j],">")==0){
+                output_redirect = 1;
+                output_file = args[j+1];
+                args[j] = NULL;
+            }
+        }
+
+
         pid_t pid = fork();
         if(pid < 0){
             perror("Fork failed");
@@ -72,14 +98,37 @@ int main(){
         }
 
         if(pid == 0){
-            
-            execvp(args[0],args);
 
+            if(input_redirect){
+                int fd = open(input_file,O_RDONLY);
+                if(fd < 0){
+                    perror("Input file error");
+                    exit(1);
+                }
+                dup2(fd,STDIN_FILENO);
+                close(fd);
+            }
+            
+            if(output_redirect){
+                int fd = open(output_file,O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                if(fd < 0){
+                    perror("Output file error");
+                    exit(1);
+                }
+                dup2(fd,STDOUT_FILENO);
+                close(fd);
+            }
+            execvp(args[0],args);
             perror("Execution failed");
             exit(1);
         }
         else{
-            wait(NULL);
+            if(!background){
+                wait(NULL);
+            }
+            else{
+                printf("[Background PID : %d]\n",pid);
+            }
         }
 
     }
